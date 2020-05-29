0db6ce272c141f518d0efb24ae79dd4f2b1d9509
T.Oka <chita123daa@gmail.com>
2020-05-29 15:59:00 +0900

    minitest sample

--- lib/sample_test.rb
+++ lib/sample_test.rb
@@ -3,5 +3,7 @@ require 'minitest/autorun'
 class SampleTest < Minitest::Test
   def test_sample
     assert_equal 'RUBY', 'ruby'.upcase
+    # assert_equal 'RUBY', nil.upcase
+    # assert_equal 'RUBY', 'ruby'.capitalize
   end
 end
