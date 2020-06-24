ef29bd16bf8d431e1c2c703bcb3921b094ed0de1
T.Oka <chita123daa@gmail.com>
2020-06-24 13:45:20 +0900

    4.8.8まで

--- lib/array_exam.rb
+++ lib/array_exam.rb
@@ -135,3 +135,38 @@ p fruits.map.with_index {|fruit, i| "#{i}: #{fruit}" }
 
 fruits = ['apple', 'orange', 'melon']
 p fruits.delete_if.with_index {|fruit, i| fruit.include?('a') && i.odd? }
+
+# 添え字を0以外の数値から開始させる 1: 2: 3:
+fruits = ['apple', 'orange', 'melon']
+# eachで繰り返しつつ1から始まる
+fruits.each.with_index(1) {|fruits, i| puts "#{i}: #{fruits}" }
+# mapで処理しつつ10かr始まる
+p fruits.map.with_index(10) {|fruit, i| "#{i}: #{fruit}" }
+
+# 配列がブロック引数に渡される場合
+dimentions = [
+  [10, 20],
+  [30, 40],
+  [50, 60],
+]
+# 面積の試算結果を格納する配列
+areas = []
+# 配列の要素分だけブロック引数を用意すると、各要素の値が別々の変数に格納される
+dimentions.each do |length, width|
+  areas << length * width
+end 
+p areas
+
+# ブロック引数を()で囲んで配列の要素を別々の引数として受け取る
+dimentions = [
+  [10, 20],
+  [30, 40],
+  [50, 60],
+]
+dimentions.each_with_index do |(length, width), i|
+  puts "length: #{length}, width: #{width}, i:#{i} "
+end
+
+# ブロックの後ろにメソッドを用いる
+names = ['田中', '佐藤', '鈴木']
+p names.map {|name| "#{name}さん" }.join('と') 
